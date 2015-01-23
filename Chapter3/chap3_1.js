bd3f529e009dcedcf081860e6ef19ece1b036032
Brian Capouch <brianc@saintjoe.edu>
2015-01-23 03:12:25 -0500

    Fix bugs and include code missing from book repo

--- Chapter3/chap3_1.js
+++ Chapter3/chap3_1.js
@@ -10,7 +10,7 @@ function List() {
    this.remove = remove;
    this.front = front;
    this.end = end;
-   this.prev = prev;
+   this.previous = previous;
    this.next = next;
    this.length = length;
    this.currPos = currPos;
@@ -46,10 +46,78 @@ function toString() {
     return this.dataStore;
 }
 
+function clear() {
+ delete this.dataStore;
+ this.dataStore = [];
+ this.listSize = this.pos = 0;
+}
+
+function front() {
+ this.pos = 0;
+}
+
+function previous() {
+ return this.dataStore[--this.pos];
+}
+
+function next() {
+ return this.dataStore[this.pos++];
+}
+
+function hasNext() {
+ if (this.pos > this.listSize -1) {
+ return false;
+ } else {
+ return true;
+ }
+}
+
+function hasPrevious() {
+ if (this.pos <= 0) {
+ return false;
+ } else {
+ return true;
+ }
+}
+function end() {
+ this.pos = this.listSize - 1;
+}
+
+function currPos() {
+ return pos;
+}
+function insert(element, after) {
+ var insertPos = this.find(after);
+ if (insertPos > -1) {
+   this.dataStore.splice(insertPos+1, 0, element);
+   ++this.listSize;
+   return true;
+ }
+   return false;
+}
+
+function length() {
+ return this.listSize;
+}
+
+function moveTo(position) {
+ this.pos = position;
+}
+
+function getElement() {
+ return this.dataStore[this.pos];
+}
+
 var names = new List();
 names.append("Cynthia");
 names.append("Raymond");
 names.append("Barbara");
+
+names.next();
+print(names.next()); // displays Raymond
+names.next();
+names.previous();
+print(names.previous()); // displays Raymond
 print(names.toString());
-names.remove("Raymond");
-print(names.toString());
+// names.remove("Raymond");
+// print(names.toString());
